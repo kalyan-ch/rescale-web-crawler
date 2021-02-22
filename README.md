3e7cf38f442aaf51cc05c38fb91c314ee35a0199
Madhuri Chavali <madhuri.chavali94@gmail.com>
2021-02-21 23:45:43 -0700

    completed basic cralwer

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # rescale-web-crawler
 basic web crawler
+
+
+## How to run
+
+run this command with url of your choice
+
+```shell
+
+python rescale-web-crawler.py https://www.rescale.com
+
+```
--- rescale-web-crawler.py
+++ rescale-web-crawler.py
@@ -1,12 +1,36 @@
-import requests
+import requests, sys
+from bs4 import BeautifulSoup
 
 def getUrls(siteUrl):
-    urls = []
+
 
     page = requests.get(siteUrl)
-    print(page.content)
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    urls = []
+    for link in soup.find_all('a'):
+        url = link.get('href')
+        if (url is not None) and ("http" in url):
+            urls.append(url)
+
+
+    urls = set(urls)
+
+    print(siteUrl)
+    for url in urls:
+        print(" ", url)
+
     return urls
 
 if __name__ == '__main__':
-    print(getUrls("https://www.rescale.com"))
+    siteUrl = sys.argv[1]
+    urls = []
+
+    urls.append(siteUrl)
+
+    i = 0
+    while len(urls) != 0:
+        newurls = getUrls(urls[i])
+        urls.extend(newurls)
+        i += 1 
 
